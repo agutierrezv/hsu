799df5d45bf853391b3a766030f7142db867a917
agutierrezv <agutierrezv@ayesa.com>
2015-11-12 14:44:37 +0100

    version 1.50
    Update connection to DB

--- app/server.js
+++ app/server.js
@@ -73,7 +73,7 @@ mongoose.connect(dbCnx);
 
 function resolveMongoDbCnx() {
 //    var localDevelDatabase = 'mongodb://localhost:27017/hsu';
-    var localDevelDatabase = 'mongodb://hsu:hsu@ds039251.mongolab.com:39251/heroku_app34785710';
+    var localDevelDatabase = 'mongodb://heroku_app34785710:65i514rkvk85i0mvtsacg29iue@ds039251.mongolab.com:39251/heroku_app34785710';
     if (process.env.VCAP_SERVICES) {
         var vCap = JSON.parse(process.env.VCAP_SERVICES);
 
