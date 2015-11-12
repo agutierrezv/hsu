f26c98826910e8b41c2955be139acca7e9f77362
agutierrezv <agutierrezv@ayesa.com>
2015-11-12 13:53:51 +0100

    version 1.50

--- app/conf/configuration.js
+++ app/conf/configuration.js
@@ -8,8 +8,8 @@ function getConfiguration(environment)
 			environment: 				'production',
 			hostUrl: 					'https://hsu.herokuapp.com', 					
 			queue: {
-				IRON_MQ_PROJECT_ID: 	'54f851267af4ab0009000055',
-				IRON_MQ_TOKEN: 			'fmtXe9gTevB0zDk4LWiEIIxy6H8'
+				IRON_MQ_PROJECT_ID: 	'55099f33c3d0ef0009000092',
+				IRON_MQ_TOKEN: 			'K8eTieqkWOr5r02CbXLqx0Vrkzs'
 	 		},
 	 		redis: {
 	 			REDISCLOUD_URL: 		'redis://rediscloud:IsbbjWjn3Py1gAkF@pub-redis-16434.eu-west-1-1.2.ec2.garantiadata.com:16434'
